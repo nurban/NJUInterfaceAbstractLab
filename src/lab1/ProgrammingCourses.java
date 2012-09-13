37e8f2454cb1135ed05b0f82dbc2b5db2a65c356
nurban <nurban@L11711038612.wctc.msft>
2012-09-12 19:02:38 -0500

    Initial Commit

---
+++ src/lab1/ProgrammingCourses.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+/**
+ *
+ * @author nurban
+ */
+public abstract class ProgrammingCourses {
+    private String courseName;
+    private String courseNumber;
+    private double credits;
+    
+    
+}
